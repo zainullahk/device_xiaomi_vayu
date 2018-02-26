ab8d7792b4b49b1a14b2b09e715aac560c5d259f
Shubham Das <dasshubham762@gmail.com>
2018-02-26 18:37:54 +0530

    vayu: Add MIUI Poweroff Charging Animation
    Surprisingly it is working very well while offline charging, same as MIUI
    
    Reference- https://review.lineageos.org/#/c/205385/
    
    Change-Id: I1282a368bc9b0b9cd51cbbcde8d9973dce822de6

---
+++ rootdir/charger/charger.mk
@@ -0,0 +1,23 @@
+DEVICE_PATH := device/xiaomi/vayu
+
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/rootdir/charger/chargeonlymode:$(TARGET_COPY_OUT_SYSTEM)/bin/charger \
+    $(DEVICE_PATH)/rootdir/charger/battery_image_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_0.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_image_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_1.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_image_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_2.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_image_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_3.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_image_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_4.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_image_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_5.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_image_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_6.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_image_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_image_7.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_num_0.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_0.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_num_1.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_1.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_num_2.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_2.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_num_3.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_3.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_num_4.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_4.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_num_5.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_5.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_num_6.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_6.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_num_7.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_7.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_num_8.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_8.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_num_9.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_9.png \
+    $(DEVICE_PATH)/rootdir/charger/battery_num_10.png:$(TARGET_OUT_DIR)/root/res/images/charger/battery_num_10.png
